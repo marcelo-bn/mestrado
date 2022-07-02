a616b026de7a1eb13b3b1ad7446ab28c5de5f98f
marcelo-bn <marcelobittencourt.eng@gmail.com>
2022-07-02 18:45:45 -0300

    atualizando req

--- inmet-tcn-project/main.py
+++ inmet-tcn-project/main.py
@@ -4,10 +4,15 @@
     TCN.
 '''
 
-# python imports
+'''' Python imports  '''
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from tcn import TCN, tcn_full_summary
+from keras.layers import Dense
+from keras.models import Sequential
 
-# own modules imports 
+'''' Own modules imports  '''
 from predict.tcn_predict import TCNpredict
 from models.tcn_model import TCNmodel
 from format.tcn_format import TCNformat
